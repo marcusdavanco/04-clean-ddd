87d0114c43b6676f5e4fbd38a32f50ee1b0f6038
Marcus Davanço <md.daves@gmail.com>
2023-11-11 17:16:00 -0300

    feat: Path aliases e Vitest globals

--- src/domain/entities/student.ts
+++ src/domain/entities/student.ts
@@ -1,7 +1,7 @@
 import { randomUUID } from "node:crypto"
-import { Entity } from "../../core/entities/entity"
-import { Optional } from "../../core/types/optional"
-import { UniqueEntityID } from "../../core/entities/unique-entity-id"
+import { Entity } from "@/core/entities/entity"
+import { Optional } from "@/core/types/optional"
+import { UniqueEntityID } from "@/core/entities/unique-entity-id"
 
 interface StudentProps {
  name: string
